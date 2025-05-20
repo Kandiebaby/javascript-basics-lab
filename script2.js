1001c62cd320b6f588237cd341a4fe2a9bc04532
Jodian <jodiankennedy@yahoo.com>
2025-05-19 23:08:37 -0400

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,40 @@
+// console.log ("Hello, World!");
+
+let myName = "Kady";
+let myAge = 25;
+// console.log(`My name is ${myName} and I am ${myAge} years old.`);
+
+let kady = true;
+let jess = false;
+// console.log(kady && jess); // Logical AND
+// console.log(kady || jess); // Logical OR 
+// console.log(!kady); // Logical NOT
+
+
+
+// let firstName = "Jessie";
+// let middleName = 'Liah';
+// let lastName = `Johnson`;
+// console.log(firstName);
+
+let fruits = ['Apple', 'Banana', 'Cherry'];
+console.log(fruits[0]); // Accesses the first element, output: 'Apple'      
+// colors.push('Berry'); // Adds a new element at the end
+console.log(fruits); // Output: ['Apple', 'Banana', 'Cherry', 'Berry']
+// fruits.pop();
+console.log(fruits); // Output: ['Apple', 'Banana', 'Cherry']
+
+
+// console.log(10 * 3); // Multiplication
+// console.log(10 / 2); // Division
+// console.log(11 % 3); // Modulus (Remainder)
+
+// console.log(3 + 3); // Addition
+// console.log(8 - 4); // Subtraction
+
+
+let a = true;
+let b = false;
+console.log(a && b); // Logical AND
+console.log(a || b); // Logical OR
+console.log(!a); // Logical NOT
